b02749b909dfe8504d03856d32bf9a8917c9b57f
theotsori <theodoretsori@gmail.com>
2023-03-27 08:46:11 +0300

    MOre engines

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is the state module."""
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
