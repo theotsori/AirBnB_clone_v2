496d99f87082df3cdd7b19cac97bcfcb7b868d63
theotsori <theodoretsori@gmail.com>
2023-04-07 10:38:55 +0000

    Theres no pswd

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to your web servers
-"""
-from datetime import datetime
-from fabric.api import env, put, run
+"""Fabric script that distributes an archive to your web servers"""
+
+from fabric.api import env, run, put
 import os
 
 env.hosts = ['54.144.138.53', '35.168.7.192']
@@ -11,23 +9,31 @@ env.hosts = ['54.144.138.53', '35.168.7.192']
 
 def do_deploy(archive_path):
     """Distributes an archive to your web servers"""
+
     if not os.path.exists(archive_path):
         return False
 
-    try:
-        put(archive_path, "/tmp/")
+    filename = os.path.basename(archive_path)
+    name_only = os.path.splitext(filename)[0]
 
-        archive_filename = os.path.basename(archive_path)
-        folder_name = "/data/web_static/releases/" + archive_filename.split(".")[0]
-        run("sudo mkdir -p {}".format(folder_name))
-        run("sudo tar -xzf /tmp/{} -C {}".
-            format(archive_filename, folder_name))
+    put(archive_path, '/tmp/{}'.format(filename))
 
-        run("sudo rm /tmp/{}".format(archive_filename))
+    run('sudo mkdir -p /data/web_static/releases/{}/'.format(name_only))
 
-        run("sudo rm -rf /data/web_static/current")
+    run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+        .format(filename, name_only))
 
-        run("sudo ln -s {} /data/web_static/current".format(folder_name))
-        return True
-    except:
-        return False
+    run('sudo rm /tmp/{}'.format(filename))
+
+    run('sudo mv /data/web_static/releases/{}/web_static/* \
+        /data/web_static/releases/{}/'.format(name_only, name_only))
+    run('sudo rm -rf /data/web_static/releases/{}/web_static'
+        .format(name_only))
+
+    run('sudo rm -rf /data/web_static/current')
+
+    run('sudo ln -s /data/web_static/releases/{}/ \
+        /data/web_static/current'.format(name_only))
+
+    print("New version deployed!")
+    return True
