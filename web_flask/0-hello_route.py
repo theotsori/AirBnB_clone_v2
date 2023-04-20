accfcb4d298511ff8177a8165d480f82ce71f474
theotsori <theodoretsori@gmail.com>
2023-04-20 09:55:36 +0300

    Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a Flask web application with a single route.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Defines a route that displays 'Hello HBNB!'."""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
