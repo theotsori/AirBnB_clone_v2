654c32bdec96db371c9d6b48eed652cfb4220a1e
theotsori <theodoretsori@gmail.com>
2023-05-15 12:52:52 +0300

    states and cities v2.1

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application.
-"""
+""" Starts a Flask web application """
+
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def teardown_session(exception=None):
+    """ Remove the current SQLAlchemy session """
+    storage.close()
+
+
 @app.route('/states', strict_slashes=False)
-def states():
-    """Displays a HTML page with a list of all State objects."""
-    states = storage.all('State')
-    return render_template('9-states.html', states=states)
+def states_list():
+    """ Display a list of all states """
+    states = storage.all(State).values()
+    states_sorted = sorted(states, key=lambda s: s.name)
+    return render_template('7-states_list.html', states=states_sorted)
 
 
-@app.route('/states/<id>', strict_slashes=False)
-def cities_by_state(id):
-    """
-    Displays a HTML page with a list of all City objects linked to a State
-    object.
-    """
-    state = storage.get('State', id)
+@app.route('/states/<state_id>', strict_slashes=False)
+def state_cities(state_id):
+    """ Display a list of cities in a state """
+    state = storage.get(State, state_id)
     if state is None:
         return render_template('9-not_found.html')
-    else:
-        return render_template('9-states.html', state=state)
-
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the database session after each request."""
-    storage.close()
+    cities = sorted(state.cities, key=lambda c: c.name)
+    return render_template('9-states.html', state=state, cities=cities)
 
 
 if __name__ == '__main__':
